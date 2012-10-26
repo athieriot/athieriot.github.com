8e791568a5df2e3691b9fd164b552f517865e748
Aurélien Thieriot <a.thieriot@gmail.com>
2012-10-27 00:14:18 +0200

    Integrate resume and wordpress posts

---
+++ assets/themes/twitter/js/resume.js
@@ -0,0 +1,204 @@
+ $(document).ready(function(){
+    $('#spybar').scrollspy();
+
+    skillsChart = new Highcharts.Chart({
+       chart: {
+          renderTo: 'skills',
+          type: 'column',
+          backgroundColor: '#F5F5F5'
+       },
+       title: {text: ''},
+       subtitle: {text: ''},
+       xAxis: {
+          reversed: true,
+          min: 0,
+          title: {text: ''},
+          labels: {enabled: false}
+       },
+       yAxis: {
+          gridLineWidth: 0,
+          min: 0,
+          title: {
+             text: '',
+             align: 'high'
+          },
+          labels: {enabled: false}
+       },
+       tooltip: {
+          formatter: function() {
+             switch(this.y){ 
+                case 1: 
+                case 2: 
+                  return this.series.name + ' : Improving'; 
+                case 3: 
+                  return this.series.name + ' : Intermediate'; 
+                case 4: 
+                  return this.series.name + ' : Good knowledge'; 
+                case 5: 
+                  return this.series.name + ' : Experienced'; 
+                case 6: 
+                  return this.series.name + ' : Proficient'; 
+                break; 
+             }
+          }
+       },
+       plotOptions: {
+          column: {
+             groupPadding: 0,                   
+             dataLabels: {
+                enabled: true,
+                rotation: 290,
+                x: 2,
+                y: -30,
+                formatter: function() {return this.series.name;}
+             }
+          }
+       },
+       legend: {enabled: false},
+       credits: {enabled: false},
+       series: [
+          {name: 'TDD',          data: [2]}, 
+          {name: 'SCRUM',        data: [3]}, 
+          {name: 'Scala',        data: [3]}, 
+          {name: 'Hibernate',    data: [4]}, 
+          {name: 'Spring',       data: [4]}, 
+          {name: 'Javascript',   data: [5]}, 
+          {name: 'Java',         data: [6]},
+          {name: 'Git',          data: [5]}, 
+          {name: 'Maven',        data: [5]}, 
+          {name: 'TeamCity',     data: [4]}, 
+          {name: 'Linux',        data: [4]}, 
+          {name: 'SQL',        data: [2]}, 
+       ],
+       exporting: {
+          buttons: {
+             exportButton: {enabled: false},
+             printButton: {enabled: false}
+          }
+       }
+    });
+ 
+    timelineChart = new Highcharts.Chart({
+       chart: {
+          renderTo: 'timeline',
+          type: 'column',
+          backgroundColor: '#F5F5F5',
+          height: 500,
+          spacingTop: 0
+       },
+       title: {text: ''},
+       xAxis: {
+          title: {text: ''},
+          labels: {enabled: false}
+       },
+       yAxis: {
+          gridLineWidth: 1,
+          gridLineDashStyle: 'dash',
+          showLastLabel: false,
+          //showFirstLabel: false,
+          min: -2,
+          title: {text: ''},
+          labels: {
+             enabled: true,
+             x: 300,
+             y: -3,
+             style: {
+                color: '#6D869F',
+                fontWeight: 'bold'
+             },
+             formatter: function() {
+                if (this.value === -2) return 'School'
+                else return this.value + ' years'
+             }
+          }
+       },
+       legend: {enabled: false},
+       credits: {enabled: false},
+       tooltip: {
+          formatter: function() {
+             return this.series.name + ' : ' + this.y +' years';
+          }
+       },
+       plotOptions: {
+          column: {
+             animation: false,
+             pointWidth: 40,
+             stacking: 'normal',
+             dataLabels: {
+                rotation: 350,
+                x: 2,
+                enabled: true,
+                formatter: function() {
+                   return this.series.name;
+                },
+                x: -45,
+                align: 'right'
+             }
+          }
+       },
+       series: [{
+               name: 'Box UK',
+               data: [0.5],
+               events: {
+                   click: function(event) {
+                       $.scrollTo('#box', 300, {offset: -40})
+                   }
+               }
+           },{
+               name: 'Evergig',
+               data: [0.1],
+               events: {
+                   click: function(event) {
+                       $.scrollTo('#evergig', 300, {offset: -40})
+                   }
+               }
+           },{
+            name: 'Vidal',
+            data: [2],
+            events: {
+               click: function(event) {
+                  $.scrollTo('#vidal', 300, {offset: -40})
+              }
+            }
+          }, {
+            name: 'Safig Dataway',
+            data: [5],
+            events: {
+               click: function(event) {
+                  $.scrollTo('#safig2', 300, {offset: -40})
+              }
+            }
+          }, {
+            name: 'Safig Dataway',
+            data: [1],
+            events: {
+               click: function(event) {
+                  $.scrollTo('#safig1', 300, {offset: -40})
+              }
+            }
+          }, {
+            name: 'DESA Logiciel',
+            data: [0.5],
+            events: {
+               click: function(event) {
+                  $.scrollTo('#desa', 300, {offset: -40})
+              }
+            }
+          }, {
+            name: 'IUT Reims',
+            data: [-2],
+            color: '#EAEA86',
+            events: {
+               click: function(event) {
+                  $.scrollTo('#iut', 300, {offset: -40})
+              }
+            }
+       }],
+       exporting: {
+          buttons: {
+             exportButton: {enabled: false},
+             printButton: {enabled: false}
+          }
+       }
+    });
+ });
